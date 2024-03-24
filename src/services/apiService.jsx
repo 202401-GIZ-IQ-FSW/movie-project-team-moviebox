abe0d9d9ae7993d3ed0bc099f18bd7057c7420a9
schinak-m <shinak.x3@gmail.com>
2024-03-24 17:38:14 +0300

    fetched the data for the Movies page

--- src/services/apiService.jsx
+++ src/services/apiService.jsx
@@ -100,3 +100,17 @@ export async function fetchUpcomingMovies() {
     return [];
   }
 }
+
+export async function fetchMoviesByGenre(genreId) {
+  try {
+    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genreId}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch movies by genre');
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error('Error fetching movies by genre:', error);
+    return [];
+  }
+}
