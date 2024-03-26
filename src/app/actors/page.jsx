a0237d1784884a725de75572a2c6716b5acf1e09
Eman Jamal <emanjse@gmail.com>
2024-03-26 14:07:11 -0700

    design single actor page

--- src/app/actors/page.jsx
+++ src/app/actors/page.jsx
@@ -1,17 +1,19 @@
+"use client"
 import React from "react"
+import { useState, useEffect } from "react"
 import ActorCard from "@/components/ActorCard/ActorCard"
+import fetchAllActors from "@/services/apiService"
 
 function Actors() {
   return (
-      <main>
-        <section className="mx-10 my-5">
-          <h1 className="text-4xl font-bold">Actors.</h1>
-          <hr class="h-px mt-4 mb-8 bg-gray-200 border-0 dark:bg-orange-700" />
+    <main>
+      <section className="mx-10 my-5">
+        <h1 className="text-4xl font-bold">Actors.</h1>
+        <hr class="h-px mt-4 mb-8 bg-gray-200 border-0 dark:bg-orange-700" />
 
-          <ActorCard actor={""}/>
-
-        </section>
-      </main>
+        <ActorCard />
+      </section>
+    </main>
   )
 }
 
